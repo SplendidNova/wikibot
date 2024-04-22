c8d5e5c1b5123f612d72f581e94fd3b503e86648
SplendidNova <122144028+SplendidNova@users.noreply.github.com>
2024-04-22 10:12:44 +0800

    Update

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_PATH: 'https://mzh.moegirl.org.cn/api.php', // api.php地址
-    CM_API_PATH: 'https://commons.moegirl.org.cn/api.php',
+    API_PATH: 'https://zh.moegirl.org.cn/api.php', // 主站api地址
+    CM_API_PATH: 'https://commons.moegirl.org.cn/api.php', // 共享站api地址
     username: '',
     password: '',
 };
